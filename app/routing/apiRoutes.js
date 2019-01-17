a40d822bbce094272041ebe45bd10ee90e91b005
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-01-16 19:43:34 -0700

    servers and routes working, updated html with bootstrap forms

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var petFriends = require("./app/data/PetFriends.js");
+var PetFriends = require("../data/PetFriends.js");
 
 //GET route with url /api/friends  to display JSON of all possible friends
-router.get("/api/friends", function(req, res) {
+router.get("/api/petfriends", function(req, res) {
     res.json(data);
   });
 //POST route to /api/friends, post survey results, handle compatibility logic
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,13 +4,13 @@ var path = require("path");
 
 //default route that leads to home.html
 router.get("/", function (req, res) {
-    console.log(__dirname);
+    console.log("/ home " + __dirname);
     res.sendFile(path.join(__dirname, "../public/home.html"));
-    
 });
 
 //GET route to display survey page
 router.get("/survey", function (req, res) {
+    console.log("/survey" + __dirname);
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
